4d1a00aade07e559c62fb227e1c8d48ac2f07d60
Abu.Osama <abuosamasports@gmail.com>
2022-12-15 19:54:26 +0530

    menu

--- navOsama.js
+++ navOsama.js
@@ -3,15 +3,17 @@ function sam() {
 }
 function navbar() {
   let parent = document.getElementById("navbar");
-  
+
+
   parent.innerHTML = `
+  <label htmlFor="checker" class="line">
+  <i class="fas fa-bars" onclick="show()"></i>
+</label>
   <div class="logo">
   <img src="logo.png" alt="" />
 </div>
 
-<label htmlFor="checker" class="line">
-  <i class="fas fa-bars" onclick="show()"></i>
-</label>
+
 <ul>
   <div class="item">
     <li onclick="sam()">
@@ -30,8 +32,36 @@ function navbar() {
     </li>
   </div>
 
-  <div>
-    <h1>osama</h1>
+  <div class="menu">
+  <p onclick="page()">NEW</p>
+  <p onclick="page()">PARTY</p>
+  <P onclick="page()">EDITION</P>
+  <P onclick="page()">ZARA ORIGINS</P>
+  <P onclick="page()">ZARA ATHLETICZ</P>
+  <P onclick="page()">BEST SELLERS</P>
+  <P onclick="page()">BASIC</P>
+  <P onclick="page()">COATS | TRENCH COATS</P>
+  <P onclick="page()">JACKETS</P>
+  <P onclick="page()">PUFFERS</P>
+  <P onclick="page()">HOODIES | SWAEATSHIRST</P>
+  <P onclick="page()">SWEATERS | CARDIGANS</P>
+  <P onclick="page()">T-SHIRSTS</P>
+  <P onclick="page()">POLO SHIRSTS</P>
+  <P onclick="page()">JEANS</P>
+  <P onclick="page()">OVERSHIRTS</P>
+  <P onclick="page()">GILETS</P>
+  <P onclick="page()">BLAZERS</P>
+  <P onclick="page()">SUITS</P>
+  <P onclick="page()">TRACKSUITS</P>
+  <P onclick="page()">SHOES</P>
+  <P onclick="page()">BAGS | BACKPACKS</P>
+  <P onclick="page()">ACCESSORIES</P>
+  <P onclick="page()">PERFUMES</P>
+  
+
+
+
+
   </div>
 </ul>
    
@@ -45,5 +75,9 @@ function show() {
     else{
         document.querySelector("ul").classList.add("shownav")
     }
+}
+function page() {
+  window.location.href="Best_sellers.html"
+  
 }
 
