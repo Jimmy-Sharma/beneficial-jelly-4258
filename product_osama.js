8f3d8d502ace53c2cc7d07912b8d2ea8f64ec85a
Abu.Osama <abuosamasports@gmail.com>
2022-12-17 13:59:24 +0530

    function

--- product_osama.js
+++ product_osama.js
@@ -1,23 +1,23 @@
 async function loadJSON(url) {
-    const res = await fetch(url);
-    return await res.json();
-  }
-  
-  loadJSON("./women.json").then((data) => {
-    product(data.women)
-  });
-  
+  const res = await fetch(url);
+  return await res.json();
+}
+let show_page = localStorage.getItem("current_page");
+loadJSON(`./${show_page}.json`).then((data) => {
+  product(data[show_page]);
+});
+// console.log(localStorage.getItem("current_page"));
+
 
 let parent = document.getElementById("parent");
 function product(data) {
   data.forEach((element) => {
-    console.log(element.item[0].img1);
-    let box_div=document.createElement("div")
-    box_div.addEventListener("click",function(){
-      bag()
-
-    })
-    box_div.innerHTML=`
+    // console.log(element.item[0].img1);
+    let box_div = document.createElement("div");
+    box_div.addEventListener("click", function () {
+      bag(element);
+    });
+    box_div.innerHTML = `
     <div class="img-box minbox">
       <img
         class="p_image"
@@ -46,14 +46,34 @@ function product(data) {
         alt=""
       />
     </div>
-    `
-    let name=document.createElement("h1")
-    name.innerText=element.name
+    `;
+    let name = document.createElement("h1");
+    name.innerText = element.name;
 
-  parent.append(box_div,name)  
+    parent.append(box_div, name);
   });
-  
 }
-function bag(){
-  window.location.href="detail_osama.html"
+
+function bag(element) {
+  console.log(element);
+localStorage.setItem("show_product",JSON.stringify(element))
+
+  window.location.href = "detail_osama.html";
+
+
 }
+function showmens() {
+  localStorage.setItem("current_page","mens")
+  window.location.href="index.html"
+}
+
+function showwomens() {
+  localStorage.setItem("current_page","women")
+  window.location.href="index.html"
+}
+
+function showkids() {
+  localStorage.setItem("current_page","kids")
+  window.location.href="index.html"
+}
+
